9702cea196daafc6cba7e9e92929cb5641bece10
jahanzaib04 <jahanzaibalikhan04@gmail.com>
2022-03-13 10:23:17 +0500

    All Files Upload

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCh_SKJadvHsP8rlMTxGJJYLjk3axjdOpU',
-  authDomain: 'clone-9b6bf.firebaseapp.com',
-  projectId: 'clone-9b6bf',
-  storageBucket: 'clone-9b6bf.appspot.com',
-  messagingSenderId: '446843253172',
-  appId: '1:446843253172:web:fe53b814f7112f95b342e1',
+  apiKey: 'AIzaSyCZgx1NSVPO8pkp7Qiy6jggGfOiXuMVHaY',
+  authDomain: 'clone-ac62f.firebaseapp.com',
+  projectId: 'clone-ac62f',
+  storageBucket: 'clone-ac62f.appspot.com',
+  messagingSenderId: '569412099886',
+  appId: '1:569412099886:web:d6a2bb6b0a28f5f85ea4bf',
 };
 
 firebase.initializeApp(firebaseConfig);
